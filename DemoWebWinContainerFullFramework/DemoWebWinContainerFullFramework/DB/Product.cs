e9776894ab9fa6ede1a14d245939e7f434870f28
Priyakant Patel <priyakantpatel@users.noreply.github.com>
2018-08-02 23:15:29 -0500

    Demo prep (#1)
    * db test added
    
    * script updated

---
+++ DemoWebWinContainerFullFramework/DemoWebWinContainerFullFramework/DB/Product.cs
@@ -0,0 +1,21 @@
+namespace DemoWebWinContainerFullFramework.DB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Product")]
+    public partial class Product
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int Id { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
